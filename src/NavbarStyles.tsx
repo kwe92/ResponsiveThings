fe94bc0b5d187464eb1a05f7bc4607119894040a
Kweayon  Clark <Kweayon@gmail.com>
2022-12-31 17:20:07 -0600

    Navbar Styled Components

---
+++ src/NavbarStyles.tsx
@@ -0,0 +1,89 @@
+import React, { Fragment } from "react";
+import styled from "styled-components";
+
+export const Navebar = styled.nav`
+
+  display: flex;
+
+  align-items: center;
+
+  justify-content: space-between;
+
+  padding: 20px;
+
+  background-color: #3259b3;
+
+  color: #fff;
+
+  a {
+    color: white;
+  }
+`;
+
+export const UlNavLinks = styled.ul`
+
+  color: #fff;
+`;
+
+export const Logo = styled.div`
+
+  font-size: 2rem;
+`;
+
+export const Menu = styled.div`
+
+  display: flex;
+
+  gap: 1rem;
+
+  font-size: 1.25rem;
+
+  li {
+    padding: 5px 14px;
+  }
+
+  li:hover {
+
+    background-color: rgba(25, 29, 36, 0.6);
+
+    transition: 0.3s ease;
+
+    border-radius: 5px;
+
+  }
+`;
+
+export const ServiceWrapper = styled.a`
+
+position: relative;
+
+`;
+
+export const DropdownWrapper = styled.div`
+`;
+
+
+export const StyledDropdown = styled.ul`
+
+background-color: #3259b3;
+
+padding: 1em 0;
+
+position: absolute; // With respect to parent
+
+display: none;
+
+border-radius: 8px;
+
+top: 35px;
+
+li + li {
+  margin-top: 10px;
+}
+
+${DropdownWrapper}:hover & {
+  display: block;
+}
+`;
+
+
