8ce31ef956a0f6635e395aa491b1afe6590e1e6a
Kweayon  Clark <Kweayon@gmail.com>
2022-12-31 17:18:18 -0600

    Root

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import App from "./App";
+import "./index.css"
+
+const ele = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(ele);
+
+root.render(
+<React.StrictMode>
+<App/>
+</React.StrictMode>
+    );
+
